3a887b67bdc4f3d0ab239eaa7095fd481dca52e4
harisaikaja <harisai@newsclick.in>
2021-02-08 16:31:39 +0530

    added table component and csv data

---
+++ src/components/table/TableWidget.js
@@ -0,0 +1,10 @@
+const TableWidget = ({ assemblyData }) => {
+    console.log('assembly',assemblyData)
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default TableWidget
--- src/pages/elections.js
+++ src/pages/elections.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
+import { csvParse } from 'd3-dsv'
 import MapDashboard from "../components/maps/MapDashboard"
+import TableWidget from "../components/table/TableWidget"
 
 /**
  * Map Page
@@ -8,26 +10,35 @@ import MapDashboard from "../components/maps/MapDashboard"
 const Elections = () => {
   const [stateGeojson, setStateGeojson] = useState([])
   const [districtGeojson, setDistrictGeojson] = useState([])
+  const [assemblyData, setAssemblyData] = useState([])
 
   useEffect(() => {
     const fetchStateGeojson = () => {
-      fetch(`/data/states.geojson`)
+      fetch(`/data/geojson/states.geojson`)
         .then((res) => res.json())
         .then(setStateGeojson)
     }
     const fetchDistrictGeojson = () => {
-      fetch(`/data/districts.geojson`)
+      fetch(`/data/geojson/districts.geojson`)
         .then((res) => res.json())
         .then(setDistrictGeojson)
     }
+    const fetchTableCsvData = () => {
+      fetch(`/data/csv/assembly_2014.csv`)
+        .then((res) => res.text())
+        .then(csvParse)
+        .then(setAssemblyData)
+    }
     fetchStateGeojson()
     fetchDistrictGeojson()
+    fetchTableCsvData()
   }, [])
-  if (stateGeojson.length === 0 || districtGeojson.length === 0) {
+  if (stateGeojson.length === 0 || districtGeojson.length === 0 || assemblyData.length === 0) {
     return <h1>Loading</h1>
   } else {
     return (
       <div>
+        <TableWidget assemblyData={assemblyData} />
         <MapDashboard
           stateGeojson={stateGeojson}
           districtGeojson={districtGeojson}
