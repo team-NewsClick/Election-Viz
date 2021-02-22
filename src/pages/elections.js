f73c5e42702172a69389821ad78c4f85644cb44d
MVS Sriharsha <sriharshamvs@gmail.com>
2021-02-22 18:56:00 +0530

    added react-select & limit fetch calls based on years

--- src/pages/elections.js
+++ src/pages/elections.js
@@ -1,33 +1,40 @@
-// import { getJsonData } from "../lib/loadCSV"
 import { useEffect, useState } from "react"
 import { csvParse } from "d3-dsv"
-
+import Select from "react-select"
+import makeAnimated from "react-select/animated"
 // import MapDashboard from "../components/maps/MapDashboard"
-
-/**
- * Generate Static Props
- * @return {Array.<Object>} Array of JSON Objects
- */
-// export async function getStaticProps() {
-//   const allJsonData = await getJsonData()
-//   return {
-//     props: {
-//       allJsonData
-//     }
-//   }
-// }
 
 /**
  * Map Page
  * @return {JSX.Element} Map Page
  */
-// const Elections = ({ allJsonData }) => {
 const Elections = () => {
   const [stateGeojson, setStateGeojson] = useState([])
   const [districtGeojson, setDistrictGeojson] = useState([])
   const [selectedYear, setSelectedYear] = useState("2019")
+  const [allYears, setAllYears] = useState([])
   const [yearByData, setYearByData] = useState([])
   const [allData, setAllData] = useState([])
+
+  const defaultSelect = "2019"
+  const dropDownOptions = [
+    {
+      value: "2019",
+      label: "2019"
+    },
+    {
+      value: "2016",
+      label: "2016"
+    },
+    {
+      value: "2015",
+      label: "2015"
+    },
+    {
+      value: "2014",
+      label: "2014"
+    }
+  ]
 
   useEffect(() => {
     const fetchStateGeojson = () => {
@@ -53,58 +60,38 @@ const Elections = () => {
         .then(
           setAllData([...allData, { year: selectedYear, data: yearByData }])
         )
-        .then(setYearByData([]))
-      // setAllData((allData) => [...allData, yearByData])
+      setAllYears([...allYears, selectedYear])
     }
-    fetchYear()
-    console.log("YearByData: ", yearByData)
+    let yearBool = allYears.includes(selectedYear)
+    if (!yearBool) {
+      fetchYear()
+    }
     console.log("All Data: ", allData)
+    console.log("All Years: ", allYears)
   }, [selectedYear])
 
-  const _handleYear = (e) => {
-    setSelectedYear(e.currentTarget.value)
+  const _handleSelectChange = (e) => {
+    if (e) {
+      console.log(e)
+      setSelectedYear(e.value)
+    }
   }
-
   if (stateGeojson.length === 0 || districtGeojson.length === 0) {
     return <h1>Loading</h1>
   } else {
     return (
       <div>
         <div className="flex justify-center">
-          <div className="radio-toolbar m-2">
-            <input
-              type="radio"
-              id="2014"
-              name="year"
-              value="2014"
-              onChange={(e) => _handleYear(e)}
+          <div className="container max-w-xl mx-auto">
+            <Select
+              components={makeAnimated}
+              placeholder="Select a Year"
+              name="selectOptions"
+              onChange={_handleSelectChange}
+              defaultValue={defaultSelect}
+              options={dropDownOptions}
+              isSearchable
             />
-            <label htmlFor="2014">2014</label>
-            <input
-              type="radio"
-              id="2016"
-              name="year"
-              value="2016"
-              onChange={(e) => _handleYear(e)}
-            />
-            <label htmlFor="2016">2016</label>
-            <input
-              type="radio"
-              id="2015"
-              name="year"
-              value="2015"
-              onChange={(e) => _handleYear(e)}
-            />
-            <label htmlFor="2015">2015</label>
-            <input
-              type="radio"
-              id="2019"
-              name="year"
-              value="2019"
-              defaultChecked
-              onChange={(e) => _handleYear(e)}
-            />
-            <label htmlFor="2019">2019</label>
           </div>
         </div>
         {/* <MapDashboard
