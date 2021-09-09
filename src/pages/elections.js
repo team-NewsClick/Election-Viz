f9c9266571b4118735c8d2d0aa4cc3118efd1b3f
sriharshamvs <sriharshamvs@gmail.com>
2021-09-09 16:27:53 +0530

    fix: browser window not defined

--- src/pages/elections.js
+++ src/pages/elections.js
@@ -26,7 +26,6 @@ export async function getStaticProps() {
   }
 }
 
-
 /**
  * Dynamic loading, component won't even be rendered on the server-side
  * @return {JSX.Element} Dashboard
@@ -78,26 +77,38 @@ const Elections = ({
   //     </div>
   //   )
   // }
-  return (
-    <div className="grid grid-cols-12">
-      <Head>
-        <title>Indian Election Maps</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <div className="col-span-2 sm:inline-block hidden"></div>
-      <div className="col-span-12 mx-5 md:col-span-8 sm:mx-0">
-        <Dashboard
-          stateGeojson={stateGeojson}
-          assemblyConstituenciesGeojson={assemblyConstituenciesGeojson}
-          parliamentaryConstituenciesGeojson={
-            parliamentaryConstituenciesGeojson
-          }
-        />
+  if (process.browser) {
+    // Client-side-only code
+    return (
+      <div className="grid grid-cols-12">
+        <Head>
+          <title>Indian Election Maps</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <div className="col-span-2 sm:inline-block hidden"></div>
+        <div className="col-span-12 mx-5 md:col-span-8 sm:mx-0">
+          <Dashboard
+            stateGeojson={stateGeojson}
+            assemblyConstituenciesGeojson={assemblyConstituenciesGeojson}
+            parliamentaryConstituenciesGeojson={
+              parliamentaryConstituenciesGeojson
+            }
+          />
+        </div>
+        <div className="col-span-2 sm:inline-block hidden"></div>
       </div>
-      <div className="col-span-2 sm:inline-block hidden"></div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div style={{ margin: "auto", paddingTop: "50vh" }}>
+        <Loading />
+      </div>
+    )
+  }
 }
 
 export default Elections
