16a57466ef14943034d3465e671df895b590e333
sriharshamvs <sriharshamvs@gmail.com>
2021-09-09 10:18:31 +0530

    deps: update for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require("path")
 
 module.exports = {
-  // target: "serverless",
+  target: "serverless",
   webpack: (config) => {
     config.resolve.alias["~"] = path.resolve(__dirname, "src")
     return config
