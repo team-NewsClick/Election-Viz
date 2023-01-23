b67452f5f068ccc4a7ea1d9b932a5600ab067481
Bappa Sinha <bappasinha@gmail.com>
2023-01-23 12:43:40 +0530

    Updated with Election commission results for Gujarat and Himachal Pradesh.

--- src/constants.js
+++ src/constants.js
@@ -11,7 +11,7 @@ export const ELECTION_VIEW_TYPE_ASSEMBLY = "assembly"
 /**
  * Default selected election
  */
-export const ELECTION_DEFAULT_SELECT = { type: "assembly", year: "Live" }
+export const ELECTION_DEFAULT_SELECT = { type: "assembly", year: "Upcoming" }
 
 /**
  * Option when no particular State/UT is selected
@@ -41,7 +41,7 @@ export const LIVE_ELECTION = "Live"
 /**
  * Live Election Year
  */
-export const LIVE_ELECTION_YEAR = "2022"
+export const LIVE_ELECTION_YEAR = "2023"
 
 /**
  * Live Election Type
@@ -52,8 +52,9 @@ export const LIVE_ELECTION_TYPE = "assembly"
  * Live Election States/UTs
  */
 export const LIVE_ELECTION_STATEUT = [
-  "Himachal Pradesh",
-  "Gujarat"
+  "Meghalaya",
+  "Nagaland",
+  "Tripura"
 ]
 
 /**
@@ -64,7 +65,7 @@ export const UPCOMING_ELECTION_TYPE = "assembly"
 /**
  * Upcoming Election Year
  */
-export const UPCOMING_ELECTION_YEAR = "2017"
+export const UPCOMING_ELECTION_YEAR = "2018"
 
 /**
  * Upcoming Election
@@ -75,8 +76,9 @@ export const UPCOMING_ELECTION = "Upcoming"
  * Upcoming Elections
  */
 const UPCOMING_ELECTIONS_STATEUT = [
-  "Himachal Pradesh",
-  "Gujarat"
+  "Meghalaya",
+  "Nagaland",
+  "Tripura"
 ]
 
 /**
@@ -228,9 +230,9 @@ export const ELECTION_YEAR_STATEUT = {
     2019: ["Andhra Pradesh", "Arunachal Pradesh", "Haryana", "Jharkhand", "Maharashtra", "Odisha", "Sikkim"],
     2020: ["Bihar", "NCT of Delhi"],
     2021: ["Assam", "Kerala", "Puducherry", "Tamil Nadu", "West Bengal"],
-    2022: ["Goa", "Manipur", "Punjab", "Uttar Pradesh", "Uttarakhand"],
-    // Upcoming : UPCOMING_ELECTIONS_STATEUT
-    Live: LIVE_ELECTION_STATEUT
+    2022: ["Goa", "Gujarat", "Himachal Pradesh", "Manipur", "Punjab", "Uttar Pradesh", "Uttarakhand"],
+    Upcoming : UPCOMING_ELECTIONS_STATEUT
+    // Live: LIVE_ELECTION_STATEUT
   }
 }
 
